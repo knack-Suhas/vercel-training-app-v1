0a2e3b806127b243f013e14c43a8e91754b6a4eb
Suhas R. Hankare <suhas.hankare@knacksystems.com>
2026-07-07 22:40:52 +0530

    Updated Geo Function and Added Package

--- app/api/edge-hello/route.tsx
+++ app/api/edge-hello/route.tsx
@@ -1,7 +1,9 @@
+import { geolocation } from '@vercel/functions'
 export const runtime = "edge"; // this is a Vercel Edge Function
 
 export async function GET(request: Request) {
-  const country = request?.geo?.country || "Unknown";
+  const geo = geolocation(request);
+  const country = geo.country || "Unknown";
   return Response.json({
     message: "Hello from a Vercel Edge Function!",
     yourCountry: country,
