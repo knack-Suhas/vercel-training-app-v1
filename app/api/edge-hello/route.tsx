915f929d18d15bc2e01330cfe1c1a801638075f0
Suhas R. Hankare <suhas.hankare@knacksystems.com>
2026-07-07 20:33:17 +0530

    Add edge function route

--- app/api/edge-hello/route.tsx
+++ app/api/edge-hello/route.tsx
@@ -1,7 +1,7 @@
 export const runtime = "edge"; // this is a Vercel Edge Function
 
 export async function GET(request: Request) {
-  const country = (request as { geo?: { country?: string } })?.geo?.country || "Unknown";
+  const country = request?.geo?.country || "Unknown";
   return Response.json({
     message: "Hello from a Vercel Edge Function!",
     yourCountry: country,
