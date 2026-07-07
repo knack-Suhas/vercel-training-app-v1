58bb497bc9f4eb23fbfa2fd6e8372910fda38a99
Suhas R. Hankare <suhas.hankare@knacksystems.com>
2026-07-07 23:14:11 +0530

    Use APP_NAME env variable

--- app/api/hello/route.tsx
+++ app/api/hello/route.tsx
@@ -2,7 +2,8 @@ export async function GET() {
   const data = {
     message: "Hello from a Vercel Serverless Function!",
     timestamp: new Date().toISOString(),
-    region: process.env.VERCEL_REGION || "local"
+    region: process.env.VERCEL_REGION || "local",
+    appName: process.env.APP_NAME || "local",
   };
   return Response.json(data);
 }
